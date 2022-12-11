b7e95f3ff761282c06082ef93543f0604e28d2d6
SAmirhoseinDoulabi <99248959+amirhoseindoulabi@users.noreply.github.com>
2022-12-11 17:03:46 +0330

    Add files via upload

---
+++ Contest/کیک در کوئرا/solution.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << ((n == 1 || n == 2 || n == 3 || n == 4 || n == 6 || n == 12) ? "YES" : "NO") << endl;
+
+    return 0;
+}
