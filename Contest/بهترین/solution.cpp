0dbb42daacc700fc241b8bb4332f3337dd21264e
SAmirhoseinDoulabi <99248959+amirhoseindoulabi@users.noreply.github.com>
2022-12-11 20:22:22 +0330

    Add files via upload

---
+++ Contest/بهترین/solution.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cin >> number;
+    cout << int("quera is the best platform.‍‍"[number - 1]) << endl;
+
+    return 0;
+}
