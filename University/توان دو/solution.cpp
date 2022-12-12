2233afaa3aea02edfdcdb7a95264d8bc24ab7a2d
seyed AmirHossein Afi <120304753+amirhossein-afi@users.noreply.github.com>
2022-12-12 14:39:04 +0330

    Create solution.cpp

---
+++ University/توان دو/solution.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a = 1 , n;
+    cin >> n;
+    for(int i = 0;a <= n;i++) a *= 2;
+    
+    cout << a;
+    
+    return 0 ;
+}
