e60e4982ac60740b65f7b5014fb022ed08944b24
seyed AmirHossein Afi <120304753+amirhossein-afi@users.noreply.github.com>
2022-12-12 11:47:40 +0330

    Create solution.cpp

---
+++ University/تاریخ تولد/solution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char name[4] , i ;
+    
+    cin.getline(name,5);
+
+     cout << "saal:" << name[0] << name[1] << endl;
+     cout << "maah:" << name[2] << name[3];
+    
+    return 0;
+}
