033e52c7cb7a932ec66536760518b09eee6f8a39
seyed AmirHossein Afi <120304753+amirhossein-afi@users.noreply.github.com>
2022-12-14 09:42:50 +0330

    Create solution.cpp

---
+++ Contest/تی شرت برنده/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned long long int  a , b , c , d;
+    cin >>  a >> b >> c >> d ;
+    
+    if(a >= c && b >= d) cout << "yes" ;
+    
+    else  cout << "no";
+    
+
+    return 0;
+}
